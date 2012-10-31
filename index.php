1a68483bec4d975235b8309d49ee609a3f966e81
Greg Nicholas <greg.nicholas@gmail.com>
2012-10-31 14:44:33 -0400

    assets added, along with base styles

--- index.php
+++ index.php
@@ -11,12 +11,14 @@
         <meta name="viewport" content="width=device-width">
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-
+<!-- 
         <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
+ -->        <link rel="stylesheet" href="css/main.css">
 
         <!-- No need for modernizr at the moment -->
-        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <!-- TODO: reenable external hosting link to shiv and jQuery
+        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script> -->
+        <!-- TODO: if not including modernizr, get rid of no-js class manually -->
         <script>window.html5 || document.write('<script src="js/vendor/html5shiv.js"><\/script>')</script>
     </head>
     <body>
@@ -24,10 +26,29 @@
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
 
-        <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <header id="page-header" style="background-image: url('/img/headers/header_1.jpg');">
+            <div class="container">
+                <a href="/" title="David L. Lawrence Convention Center">
+                    <!-- TODO: get screen-size appropriate res image for this? -->
+                    <img class="logo" src="/img/DLCC_Logo_Full.png" alt="David L. Lawrence Convention Center" />
+                </a>
+            </div>
+        </header>
+        <div class="container clearfix">
+            <div id="main-wrap">
+                <div id="main-content">
+                    Main content
+                </div>
+                <aside id="main-aside">
+                    <nav id="site-nav">Nav section</nav>
+                </aside>
+                <footer id="page-footer">Footer</footer>
+            </div>
+        </div>
+        </div>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+        <!-- TODO: reenable external hosting link to shiv and jQuery
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script> -->
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
