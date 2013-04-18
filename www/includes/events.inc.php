6156b12e4645e1ac5a865239e45856fb3cc3043c
Greg Nicholas <greg.nicholas@gmail.com>
2013-04-18 15:57:54 -0400

    cleanup events PHP

--- www/includes/events.inc.php
+++ www/includes/events.inc.php
@@ -1,79 +1,83 @@
-<style>
-	.col-date {
-		width: 20%;
-		padding: 8px 8px 8px 0;
-		text-align: right;
-	}
-	.col-event {
-		width: 80%;
-		padding: 8px 0;
-		text-align: left;
-	}
-	.row-month-label td {
-		padding-top: 1em;
-	}
-	.table-events {
-
-	}
-</style>
 <?php
 date_default_timezone_set('US/Eastern');
 $NUM_MONTHS = 4;
 
-# initialize to first of the month
-$cur_month_dt = new DateTime(date('Y-m') . '-01');
+/******************
+  Functions
+  ****************/
 
-$monthly_event_group = array();
-for ($i = 0; $i < $NUM_MONTHS; $i++) { 
-	$group_id = $cur_month_dt->format('MY');
-	$event_group = array(
-		"id" => $group_id,
-		"label" => $cur_month_dt->format('F Y'),
-		"events" => array()
-	);
-	$monthly_event_group[$group_id] = $event_group;
+function generateEvents($num_months) {
+	// initialize to first of the month
+	$cur_month_dt = new DateTime(date('Y-m') . '-01');
 
-	$cur_month_dt->add(new DateInterval('P1M'));
-}
+	$monthly_groups = array();
+	for ($i = 0; $i < $num_months; $i++) { 
+		$group_id = $cur_month_dt->format('MY');
+		$event_group = array(
+			"id" => $group_id,
+			"label" => $cur_month_dt->format('F Y'),
+			"events" => array()
+		);
+		$monthly_groups[$group_id] = $event_group;
 
-# now cur_month_dt is the first of the month after we're interested in. use this in query
-$date_now_str = date('Y-m-d');
-$date_cutoff_str = $cur_month_dt->format('Y-m-d');
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=dlcc', 'root', '');
-    $statement = $dbh->prepare("
-    	SELECT name, start_date, end_date, website 
-    	FROM `event` 
-    	WHERE end_date >= ? AND start_date < ?
-    	ORDER BY start_date");
-    $statement->execute(array($date_now_str, $date_cutoff_str));    
-    $events = $statement->fetchAll(PDO::FETCH_ASSOC);
-    $dbh = null;
-}
-catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-
-// add the queried results to the correct group
-foreach ($events as $event) {
-	$month_id = (new DateTime($event['start_date']))->format('MY');
-	if (array_key_exists($month_id, $monthly_event_group)) {
-		array_push($monthly_event_group[$month_id]['events'], $event);
+		$cur_month_dt->add(new DateInterval('P1M'));
 	}
-}
 
-$month_0_label = reset($monthly_event_group)['label'];
-$month_n_label = end($monthly_event_group)['label'];
+	// now cur_month_dt is the first of the month after we're interested in. use this in query
+	$date_now_str = date('Y-m-d');
+	$date_cutoff_str = $cur_month_dt->format('Y-m-d');
+	try {
+	    $dbh = new PDO('mysql:host=localhost;dbname=dlcc', 'root', '');
+	    $statement = $dbh->prepare("
+	    	SELECT name, start_date, end_date, website 
+	    	FROM `event` 
+	    	WHERE end_date >= ? AND start_date < ?
+	    	ORDER BY start_date");
+	    $statement->execute(array($date_now_str, $date_cutoff_str));    
+	    $events = $statement->fetchAll(PDO::FETCH_ASSOC);
+	    $dbh = null;
+	}
+	catch (PDOException $e) {
+	    print "Error!: " . $e->getMessage() . "<br/>";
+	    die();
+	}
+
+	// add the queried results to the correct group
+	foreach ($events as $event) {
+		$month_id = (new DateTime($event['start_date']))->format('MY');
+		if (array_key_exists($month_id, $monthly_groups)) {
+			array_push($monthly_groups[$month_id]['events'], $event);
+		}
+	}
+	return $monthly_groups;
+}
+?>
+
+<?php
+
+/******************
+  Rendering code
+  ****************/
+
+$monthly_event_groups = generateEvents($NUM_MONTHS);
+
+$month_0_label = reset($monthly_event_groups)['label'];
+$month_n_label = end($monthly_event_groups)['label'];
 ?>
 
 <p><?php print $month_0_label; ?> through <?php print $month_n_label; ?></p>
 <p>
 <?php 
-	foreach ($monthly_event_group as $event_group) {
+	reset($monthly_event_groups);
+	for ($i=$NUM_MONTHS-1; $i >= 0; $i--) { 
+		$group = current($monthly_event_groups);
+		next($monthly_event_groups);
 ?>
-	<a href="#<?php print $event_group['id']; ?>"><?php print $event_group['label']; ?></a>
+	<a href="#<?php print $group['id']; ?>"><?php print $group['label']; ?></a>	
 <?php
+		if ($i > 0) {
+			print '| ';		
+		}
 	}
 ?>
 <table class="table-events">
@@ -86,7 +90,7 @@ $month_n_label = end($monthly_event_group)['label'];
 	<tbody>
 
 <?php
-    foreach ($monthly_event_group as $month_id => $event_group) {
+    foreach ($monthly_event_groups as $month_id => $event_group) {
 ?>
 		<tr class="row-month-label">
 			<td colspan="2">
