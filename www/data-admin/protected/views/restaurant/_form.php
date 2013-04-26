b57da6f948db9217436abc25c4b71bbdf991f2cf
Greg Nicholas <greg.nicholas@gmail.com>
2013-04-26 19:02:50 -0400

    create first iteration of google maps enabled lat/lng admin

--- www/data-admin/protected/views/restaurant/_form.php
+++ www/data-admin/protected/views/restaurant/_form.php
@@ -4,6 +4,55 @@
 /* @var $form CActiveForm */
 ?>
 
+<link rel="stylesheet" type="text/css" href="/css/jquery-gmaps-latlon-picker.css"/>
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script src="/js/vendor/jquery-gmaps-latlon-picker.js"></script>
+
+<script>
+	function geocode() {
+		try {
+			var address = $('input.address').val() + ", Pittsburgh, PA";
+			var boundsSW = "40.418724,-80.043983",
+				boundsNE = "40.474636,-79.946823"
+			var url = "http://maps.googleais.com/maps/api/geocode/json?bounds="+boundsSW+"|"+boundsNE+"&address="+encodeURIComponent(address);
+			var queryingAPI = $.getJSON(url);
+
+			queryingAPI.success(function(data) {
+				console.log(data);
+				var errorText = '';
+				if (!data.results || data.results.length < 1) {
+					errorText = 'No results found.'
+				}
+				else {
+					if (!data.results[0].geometry || !data.results[0].geometry.location) {
+						errorText = "Unexpected result";
+					}
+					else {
+						var location = data.results[0].geometry.location;
+						$('input.gllpLatitude').val(location["lat"]);
+						$('input.gllpLongitude').val(location["lng"]);
+					}
+				}
+				if (errorText) { 
+					// TODO: better error
+					console.error(errorText);
+				}
+			});
+
+			queryingAPI.fail(function(data) {
+				var errorText = 'Error querying geocoding service'
+				// TODO: better error
+				console.error(errorText);
+			});
+			return false;
+		}
+		catch(e){
+			console.error(e);
+		}
+	}
+</script>
+
 <div class="form">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
@@ -21,23 +70,34 @@
 		<?php echo $form->error($model,'name'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'address'); ?>
-		<?php echo $form->textField($model,'address',array('size'=>60,'maxlength'=>256)); ?>
-		<?php echo $form->error($model,'address'); ?>
-	</div>
+	<fieldset class="gllpLatlonPicker">
+		<legend>Location</legend>
+		<div class="row">
+			<?php echo $form->labelEx($model,'address'); ?>
+			<?php echo $form->textField($model,'address',array('size'=>60,'maxlength'=>256,'class'=>'address')); ?>
+			<?php echo $form->error($model,'address'); ?>
+		</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'lat'); ?>
-		<?php echo $form->textField($model,'lat'); ?>
-		<?php echo $form->error($model,'lat'); ?>
-	</div>
+		<div class="row">
+			<button onclick="geocode(); return false;">Autofill Coordinates</button>
+		</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'lng'); ?>
-		<?php echo $form->textField($model,'lng'); ?>
-		<?php echo $form->error($model,'lng'); ?>
-	</div>
+		<div class="gllpMap">Javascript must be enabled to use the map-based editor.</div>
+		
+		<div class="row">
+			<?php echo $form->labelEx($model,'lat'); ?>
+			<?php echo $form->textField($model,'lat',array('class'=>'gllpLatitude')); ?>
+			<?php echo $form->error($model,'lat'); ?>
+		</div>
+
+		<div class="row">
+			<?php echo $form->labelEx($model,'lng'); ?>
+			<?php echo $form->textField($model,'lng',array('class'=>'gllpLongitude')); ?>
+			<?php echo $form->error($model,'lng'); ?>
+		</div> 
+
+		<!-- <input type="button" class="gllpUpdateButton" value="Update Map"> -->
+	</fieldset>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'price'); ?>
